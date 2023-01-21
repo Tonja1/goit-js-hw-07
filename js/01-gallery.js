870c00fc93f736215e71e96f6895cf956a7dba1e
Antonina Pasichnyk <110766517+Tonja1@users.noreply.github.com>
2023-01-21 18:02:07 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galeryListOfImagesEl = document.querySelector('.gallery')
+ const imageItemsMarkup = createImageMarkup(galleryItems)
+const url = returnUrlOfBiggestImg
+
+function returnUrlOfBiggestImg(evt) {
+    if (evt.target.nodeName !== 'IMG') {
+        return 
+    }
+  const bigImg = basicLightbox.create(`
+     <img src="${evt.target.dataset.source}" width="800" height="600">
+  `)
+}
+galeryListOfImagesEl.addEventListener('click', returnUrlOfBiggestImg)
+galeryListOfImagesEl.addEventListener('click', () => {
+bigImg.show()
+})
+  function createImageMarkup(items) {
+      return items.map( item =>
+      `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-sourse="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>
+    `).join("")
+  }
+galeryListOfImagesEl.innerHTML = imageItemsMarkup;
+
 console.log(galleryItems);
+
