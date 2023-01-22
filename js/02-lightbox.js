cec7877868198b07a235c249578cd20ef05ca615
Antonina Pasichnyk <110766517+Tonja1@users.noreply.github.com>
2023-01-22 13:01:52 +0200

    update2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galeryEl = document.querySelector('.gallery');
+function newGellery(image) {
+    return image
+        .map(({ preview, original, description }) => {
+            return `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+        })
+        .join("");
+}
 
+galeryEl.insertAdjacentHTML("beforeend", newGellery(gelleryItems));
 
+new Simplelightbox(".gallery a", {
+    captiosData: "alt",
+    captionsDelay: 250,
+});
 
  console.log(galleryItems);
 
